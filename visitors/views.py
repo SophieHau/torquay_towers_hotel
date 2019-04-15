36b3288fb4672f909f15fe127f1b13a3ab048e58
Gary Waller <garywaller246@gmail.com>
2019-04-15 15:43:09 +0300

    css and url links

--- visitors/views.py
+++ visitors/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
-	return HttpResponse('<h1>Home Working</h1>')
+	return render(request, 'index.html')
 
 
 def booking(request):
-	return HttpResponse('<h1>Booking Working</h1>')
+	return render(request, 'booking.html')
 
 
 def reviews(request):
-	return HttpResponse('<h1>Reviews Working</h1>')
+	return render(request, 'reviews.html')
