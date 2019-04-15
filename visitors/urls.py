487c1b27abed81c9c37f04447e0efbf728dcf511
Sophie <you@example.com>
2019-04-15 14:23:38 +0300

    adding login and logout

---
+++ visitors/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('<int:user_id>', views.index, name='index'),
+	path('accounts/login/', views.login_view, name='login'),
+	path('accounts/logout', views.logout_view, name='logout'),
+]
