d89176e20db91d0467a1c9a44148008974b7df42
Sophie <you@example.com>
2019-04-16 14:07:15 +0300

    login corrections

--- visitors/views.py
+++ visitors/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.utils import timezone
 
-
+@login_required(login_url='accounts/login/')
 def index(request):
     return render(request, 'index.html')
 
 def login_view(request):
-
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -28,3 +28,24 @@ def booking(request):
 
 def reviews(request):
 	return render(request, 'reviews.html')
+
+# def show_vacancies(request):
+#     if request.method == 'POST':
+#         start_date = request.POST['start_date']
+#         end_date = request.POST['end_date']
+#         rooms = Room.objects.all()
+#         for room in rooms:
+
+#     Can I book a stay?
+# given: X people,
+#        C start date,
+#        D end date.
+# let A = today
+# if C is before A - return No
+# we need to find all rooms which are NOT free between C and D:
+# for each day between C and D (inclusive?)
+# get all bookings which:
+#     start date is before C, 
+#         and end date is after C
+#     OR start date is on or after C
+#         and start date is before D
