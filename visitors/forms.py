18ebb6083a3899e9b74ceac48d57fa97b71619cc
Selmo <45966506+selmoka@users.noreply.github.com>
2019-04-17 10:57:11 +0300

    Fixed booking

--- visitors/forms.py
+++ visitors/forms.py
@@ -1,14 +1,9 @@
 from django import forms
 from django.forms import TextInput
 from .models import UserHotel, Room, Booking
-from bootstrap_datepicker_plus import DatePickerInput
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['room','startdate','enddate']
-        widgets = {
-            'startdate': DatePickerInput(), # default date-format %m/%d/%Y will be used
-            'enddate': DatePickerInput(format='%d-%m-%Y'), # specify date-frmat
-        }
-
+    
